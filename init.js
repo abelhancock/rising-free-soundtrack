922edf54ff7b40123d5fa307a3ac64a868c73d0b
Abel <abel.hancock@liferay.com>
2019-09-27 11:20:42 -0700

    add init.js

---
+++ init.js
@@ -0,0 +1,239 @@
+Amplitude.init({
+    songs: [
+      {
+        name: "The Beginning",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/01%20The%20Beginning.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Journey",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/02%20The%20Journey.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Girl",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/03%20The%20Girl.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Aftershock",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/04%20The%20Aftershock.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Lonely Coast",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/05%20The%20Lonely%20Coast.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Story of Nakomay",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/06%20The%20Story%20of%20Nakomay.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Hatred",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/07%20The%20Hatred.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Flashback",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/08%20The%20Flashback.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Poem",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/09%20The%20Poem.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Orphanage",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/10%20The%20Orphanage.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Story of Mother",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/11%20The%20Story%20of%20Mother.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Parlor",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/12%20The%20Parlor.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Cry",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/14%20The%20Cry.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Date",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/15%20The%20Date.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Arrival of James",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/16%20The%20Arrival%20of%20James.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Unwanted Welcome",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/17%20The%20Unwanted%20Welcome.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Brother & Sister",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/18%20The%20Brother%20%26%20Sister.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Other Brother",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/19%20The%20Other%20Brother.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Engagement",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/20%20The%20Engagement.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Sudden Attack",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/21%20The%20Sudden%20Attack.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Rescue",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/22%20The%20Rescue.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Turn & The Frontier",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/23%20The%20Turn%20%26%20The%20Frontier.mp3,
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Epilogue",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/24%20The%20Epilogue.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+      {
+        name: "The Credits",
+        artist: "Abel Hancock",
+        album: "Rising Free (Original Motion Picture Sountrack)",
+        url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/audio/25%20The%20Credits.mp3",
+        cover_art_url:
+          "https://raw.githubusercontent.com/abelhancock/rising-free-soundtrack/master/images/Rising-Free-Soundtrack-Cover.jpg"
+      },
+    ],
+
+    playlists: {
+      rising_free: {
+        songs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+        title: "Rising Free"
+      }
+    },
+
+    callbacks: {
+      timeupdate: function check(x) {
+        var songPlayedPercentage = Amplitude.getSongPlayedPercentage();
+        elements = document.querySelectorAll(
+          ".amplitude-active-song-container .amplitude-current-time"
+        );
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].style.left = songPlayedPercentage + "%";
+        }
+      }
+    }
+  });
