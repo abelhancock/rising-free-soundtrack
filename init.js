285e3c4b0768a1d6559557baff0a25fcb25ac750
Abel <abel.hancock@liferay.com>
2019-09-27 11:54:43 -0700

    rename playlist

--- init.js
+++ init.js
@@ -219,9 +219,9 @@ Amplitude.init({
     ],
 
     playlists: {
-      rising_free: {
+      risingFree: {
         songs: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
-        title: "rising_free"
+        title: "Rising Free"
       }
     },
 
